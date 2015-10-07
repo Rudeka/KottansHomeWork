89483f31b7767f773f0e5fe40c8448d9337747a2
Rudeka <thotubox@gmail.com>
2015-10-07 07:59:13 +0300

    Initial
    Implementation of cipher

---
+++ HW1/CeasarCipher/CipherImplementation/CeasarCipher.cs
@@ -0,0 +1,37 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace CipherImplementation
+//{
+//    public class CeasarCipher
+//    {
+//        private int key;
+//        public CeasarCipher(int offset)
+//        {
+//            key = offset;
+//        }
+
+//        public string Encrypt (string wordToEncrypt)
+//        {var [] alphabet = new [] {"a", "b", "c", "d", } 
+//            char[] charWord = wordToEncrypt.ToCharArray();
+//            for (int i = 0; i < charWord.Length; i++)
+//            {
+                
+//            }
+
+//            //string [] wordsArray = wordToEncrypt.Split(' ');
+//            //foreach (var word in wordsArray)
+//            //{
+//            //    char [] charWord = word.ToCharArray();
+//            //}
+//        }
+
+//        public string Decrypt(string wordToDecrypt)
+//        {
+            
+//        }
+//    }
+//}
