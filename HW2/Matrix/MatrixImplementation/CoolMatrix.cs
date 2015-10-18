f78bca9411a7ca0069765b923e85d388f44cae89
Rudeka <thotubox@gmail.com>
2015-10-19 00:19:11 +0300

    InitialHW2

---
+++ HW2/Matrix/MatrixImplementation/CoolMatrix.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.CodeDom;
+
+namespace MatrixImplementation
+{
+    public class CoolMatrix
+    {
+        private int[,] arr;
+        private Size matrixSize;
+        private bool isSquare;
+        
+        public CoolMatrix(int[,] arr)
+        {
+            if (arr == null)
+            {
+                throw new ArgumentNullException();
+            }
+            this.arr = arr;
+            matrixSize = new Size(arr.GetLength(0), arr.GetLength(1));
+            isSquare = matrixSize.IsSquare;
+        }
+        
+        public object Size => matrixSize;
+        public bool IsSquare => isSquare;
+
+        public static implicit operator CoolMatrix (int[,] size)
+        {
+            return new CoolMatrix(size);
+        }
+
+        public override string ToString()
+        {
+
+            string stringMatrix = "";
+            for (int i = 0; i <arr.Length/2; i++)
+                {
+                    var s1 = arr.GetValue(i,0);
+                    var s2 = arr.GetValue(i, 1);
+                    stringMatrix += String.Format("[{0}, " + "{1}]", s1, s2);
+                    if (i < arr.GetLength(1)-1)
+                    {
+                        stringMatrix += Environment.NewLine;
+                    }
+                }
+
+            return stringMatrix;
+        }
+    }
+}
---
+++ HW2/Matrix/MatrixImplementation/Size.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace MatrixImplementation
+{
+    public struct Size : IEquatable<Size>
+    {
+
+#region Equality members 
+        public bool Equals(Size other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return width == other.width && height == other.height && IsSquare == other.IsSquare;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != typeof (Size)) return false;
+            return Equals((Size) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                var hashCode = width;
+                hashCode = (hashCode*397) ^ height;
+                hashCode = (hashCode*397) ^ IsSquare.GetHashCode();
+                return hashCode;
+            }
+        }
+
+        public static bool operator ==(Size left, Size right)
+        {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(Size left, Size right)
+        {
+            return !Equals(left, right);
+        }
+#endregion
+
+        private int width;
+        private int height;
+
+        private bool isSquare;
+        public Size(int width, int height)
+        {
+            this.width = width;
+            this.height = height;
+            isSquare = width == height;
+        }
+
+        public int Width => width;
+        public int Height => height;
+        public bool IsSquare => isSquare;
+
+        //public bool IsSquare()
+        //{
+        //    return Width == Height;
+        //}
+    }
+}
